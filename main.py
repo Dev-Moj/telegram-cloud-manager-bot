ae0f9d4b9a623d3a80c9559d57747df44f1366a9
amiram0011 <mojtabagafarzade@gmail.com>
2023-09-08 19:30:49 +0330

    Basic telebot settings Create user table

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import telebot
+import sqlite3
+
+BOT_TOKEN = 'BOT_TOKEN'
+bot = telebot.TeleBot(BOT_TOKEN)
+
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS users
+             (user_id TEXT,
+             user_name TEXT,
+             view_name TEXT,
+             create_date DATETIME
+             )''')
+
+
+
+conn.close()
